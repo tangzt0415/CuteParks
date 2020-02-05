f2a674b308df22f6de1667750b4e16c1714a90b9
tangzt96 <tangzt96@gmail.com>
2020-02-05 12:18:03 +0800

    added retrieval of postal

---
+++ app/src/main/java/com/example/testapplication/fetchData.java
@@ -0,0 +1,43 @@
+package com.example.testapplication;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class fetchData extends AsyncTask<Void,Void,Void> {
+    private String data = "";
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        try {
+            //setup URL
+            URL url = new URL("https://developers.onemap.sg/commonapi/search?searchVal="+testActivity.postal+"&returnGeom=Y&getAddrDetails=Y&pageNum=1");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream =  httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            //read json into data
+            String line = "";
+            while(line != null){
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        testActivity.data.setText(this.data);
+    }
+
+}
--- app/src/main/java/com/example/testapplication/testActivity.java
+++ app/src/main/java/com/example/testapplication/testActivity.java
@@ -1,13 +1,19 @@
 package com.example.testapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 
 public class testActivity extends AppCompatActivity {
-
+    public static int postal;
+    Button click;
+    public static TextView data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +22,23 @@ public class testActivity extends AppCompatActivity {
         if (getIntent().hasExtra("com.example.testapplication.SOMETHING")) {
             TextView tv = (TextView) findViewById(R.id.textView2);
             //retrieving info
-            int postal = getIntent().getExtras().getInt("com.example.testapplication.SOMETHING");
+            postal = Objects.requireNonNull(getIntent().getExtras()).getInt("com.example.testapplication.SOMETHING");
             tv.setText(String.valueOf(postal));
-            
+
         }
+        click = (Button)findViewById(R.id.getCoordinate);
+        data = (TextView)findViewById(R.id.displayJson);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchData process = new fetchData();
+                process.execute();
+            }
+        });
+
+    }
+    public int getPostal(){
+        return postal;
     }
 }
