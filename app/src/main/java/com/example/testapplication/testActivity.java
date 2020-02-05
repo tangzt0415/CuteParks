b041b398cb120e61399e7c4603715c4aabffc402
tangzt96 <tangzt96@gmail.com>
2020-02-05 11:34:39 +0800

    removed broken code

--- app/src/main/java/com/example/testapplication/testActivity.java
+++ app/src/main/java/com/example/testapplication/testActivity.java
@@ -5,15 +5,6 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 
 public class testActivity extends AppCompatActivity {
 
@@ -27,33 +18,7 @@ public class testActivity extends AppCompatActivity {
             //retrieving info
             int postal = getIntent().getExtras().getInt("com.example.testapplication.SOMETHING");
             tv.setText(String.valueOf(postal));
-
-            final TextView textView = (TextView) findViewById(R.id.text);
-// ...
-
-// Instantiate the RequestQueue.
-            RequestQueue queue = Volley.newRequestQueue(this);
-            String url ="https://developers.onemap.sg/commonapi/search?searchVal=640643&returnGeom=Y&getAddrDetails=Y&pageNum=1";
-
-// Request a string response from the provided URL
-
-            JsonObjectRequest Request = new JsonObjectRequest(Request.Method.GET, url,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            // Display the first 500 characters of the response string.
-                            //textView.setText("Response is: "+ response.substring(0,500));
-                            JSONArray jsonArray = response.getJSONArray("results");
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    textView.setText("That didn't work!");
-                }
-            });
-
-// Add the request to the RequestQueue.
-            queue.add(Request);
+            
         }
     }
 }
