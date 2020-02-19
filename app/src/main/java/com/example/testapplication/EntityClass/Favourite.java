70bfeaf17b73b4c7f46f2ab96fded9a35a12e2f1
ElinWyl <42896491+ElinWyl@users.noreply.github.com>
2020-02-20 00:40:30 +0800

    Update Favourite.java
    20 Feb, by YL

--- app/src/main/java/com/example/testapplication/EntityClass/Favourite.java
+++ app/src/main/java/com/example/testapplication/EntityClass/Favourite.java
@@ -1,4 +1,42 @@
 package com.example.testapplication.EntityClass;
 
+
 public class Favourite {
+	private String userId;
+	private String parkId;
+	private String parkName;
+	
+	//////////////////////////////////////////////////////////
+	//Constructors
+	public Favourite(){
+	
+	}
+	
+	public Favourite(String userId, Park park){
+		this.userId = userId;
+		this.parkId = park.getId();
+		this.parkName = park.getName();
+	}
+	
+	//////////////////////////////////////////////////////////
+	//getters and setters
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getParkId() {
+		return parkId;
+	}
+	public void setParkId(String parkId) {
+		this.parkId = parkId;
+	}
+	public String getParkName() {
+		return parkName;
+	}
+	public void setParkName(String parkName) {
+		this.parkName = parkName;
+	}
+	
 }
