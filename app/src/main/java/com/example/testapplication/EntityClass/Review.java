d48413cb2d2e61717ae93ba2b6384f3ad333be21
ElinWyl <42896491+ElinWyl@users.noreply.github.com>
2020-02-19 23:05:59 +0800

    Update Review.java
    19 Feb, by YL

--- app/src/main/java/com/example/testapplication/EntityClass/Review.java
+++ app/src/main/java/com/example/testapplication/EntityClass/Review.java
@@ -1,4 +1,85 @@
 package com.example.testapplication.EntityClass;
 
+
+import java.util.UUID;
+
 public class Review {
+    private String id;
+    private String userId;
+    private String parkId;
+    private int rating;
+    private String description;
+
+
+    //////////////////////////////////////////////////////////
+    //Constructors
+    public Review(){
+
+    }
+
+    public Review(String userId, String parkId, int rating, String description) {
+        this.id = UUID.randomUUID().toString();
+        this.userId = userId;
+        this.parkId = parkId;
+        this.rating = rating;
+        this.description = description;
+    }
+
+
+    //////////////////////////////////////////////////////////
+    //Getters and setters
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setRandomId() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+
+    public String getParkId() {
+        return parkId;
+    }
+
+
+    public void setParkId(String parkId) {
+        this.parkId = parkId;
+    }
+
+
+    public int getRating() {
+        return rating;
+    }
+
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
 }
