02c1e06ac54b92e6e9b3ec38c6b4f665fb5f998c
ElinWyl <42896491+ElinWyl@users.noreply.github.com>
2020-02-20 21:59:36 +0800

    Update Amenity.java
    20 Feb, by YL

--- app/src/main/java/com/example/testapplication/EntityClass/Amenity.java
+++ app/src/main/java/com/example/testapplication/EntityClass/Amenity.java
@@ -1,4 +1,35 @@
 package com.example.testapplication.EntityClass;
 
+
 public class Amenity {
+    private int id;
+    private String description;
+
+
+    //////////////////////////////////////////////////////////
+    //Constructor
+    public Amenity() {
+
+    }
+
+    public Amenity(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    //////////////////////////////////////////////////////////
+    //Getters and setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
