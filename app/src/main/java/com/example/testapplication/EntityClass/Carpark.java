dcf03b6077964945bb13ec3c358cad41ed01cd1c
ElinWyl <42896491+ElinWyl@users.noreply.github.com>
2020-02-20 21:53:36 +0800

    Update Carpark.java
    20 Feb, by YL

--- app/src/main/java/com/example/testapplication/EntityClass/Carpark.java
+++ app/src/main/java/com/example/testapplication/EntityClass/Carpark.java
@@ -1,4 +1,50 @@
 package com.example.testapplication.EntityClass;
 
 public class Carpark {
+	private int id;
+	private String name;
+	private double locationX;
+	private double locationY;
+	
+	
+	//////////////////////////////////////////////////////////
+	//Constructor
+	public Carpark() {
+		
+	}
+	
+	public Carpark(int id, String name, double locationX, double locationY) {
+		this.id = id;
+		this.name = name;
+		this.locationX = locationX;
+		this.locationY = locationY;
+	}
+	
+	//////////////////////////////////////////////////////////
+	//Getters and setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getLocationX() {
+		return locationX;
+	}
+	public void setLocationX(double locationX) {
+		this.locationX = locationX;
+	}
+	public double getLocationY() {
+		return locationY;
+	}
+	public void setLocationY(double locationY) {
+		this.locationY = locationY;
+	}
+	
 }
