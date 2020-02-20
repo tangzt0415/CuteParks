4d2ecbf4ecd12f82f5656803e37d1976b7a908ef
ElinWyl <42896491+ElinWyl@users.noreply.github.com>
2020-02-20 21:38:15 +0800

    Update Park.java
    20 Feb, by YL

--- app/src/main/java/com/example/testapplication/EntityClass/Park.java
+++ app/src/main/java/com/example/testapplication/EntityClass/Park.java
@@ -4,84 +4,147 @@ package com.example.testapplication.EntityClass;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class User {
-    private String id;
-    private String name;
-    private String password;
-    private double locationX;
-    private double locationY;
+public class Park {
+	private String id;
+	private String name;
+	private String description;
+	private double locationX;
+	private double locationY;
+	private String locationAddress;
+	private String website;
+	private double overallRating;
+	private ArrayList<Amenity> Amenities;
+	private ArrayList<Carpark> NearbyCarparks;
+	private ArrayList<Review> Reviews;
+	
+	//////////////////////////////////////////////////////////
+	//Constructors
+	public Park(){
+	
+	}
+	
+	public Park(String name, String description, double locationX, double locationY, String locationAddress, String website) {
+		this.id = UUID.randomUUID().toString();
+		this.name = name;
+		this.description = description;
+		this.locationX = locationX;
+		this.locationY = locationY;
+		this.locationAddress = locationAddress;
+		this.website = website;
+		this.Amenities = new ArrayList<Amenity>();
+		this.NearbyCarparks = new ArrayList<Carpark>();
+		this.Reviews = new ArrayList<Review>();
+	}
+	
+	//////////////////////////////////////////////////////////
+	//Getters and setters
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setId() {
+		this.id = UUID.randomUUID().toString();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    //////////////////////////////////////////////////////////
-    //Constructors
-    public User(){
+	public String getDescription() {
+		return description;
+	}
 
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    //for new user sign-up
-    public User(String name, String password){
-        this.id = UUID.randomUUID().toString();
-        this.name = name;
-        this.password = password;
-        this.locationX = 0;
-        this.locationY = 0;
-    }
+	public double getLocationX() {
+		return locationX;
+	}
 
-    //for existing user log-in
-    public User(String id, String name, String password){
-        this.id = id;
-        this.name = name;
-        this.password = password;
-        this.locationX = 0;
-        this.locationY = 0;
-    }
+	public void setLocationX(double locationX) {
+		this.locationX = locationX;
+	}
 
+	public double getLocationY() {
+		return locationY;
+	}
 
-    //////////////////////////////////////////////////////////
-    //getters and setters
-    public String getId() {
-        return id;
-    }
-    
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setLocationY(double locationY) {
+		this.locationY = locationY;
+	}
 
-    public void setId() {
-        this.id = UUID.randomUUID().toString();
-    }
+	public String getLocationAddress() {
+		return locationAddress;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setLocationAddress(String locationAddress) {
+		this.locationAddress = locationAddress;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getWebsite() {
+		return website;
+	}
 
+	public void setWebsite(String website) {
+		this.website = website;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public double getOverallRating() {
+		this.setOverallRating();
+		return this.overallRating;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setOverallRating() {
+		int totalRating = 0;
+		int count = 0;
+		for (Review review:this.Reviews){
+			totalRating += review.getRating();
+			count ++;
+		}
+		if (count != 0) {
+			this.overallRating = (double)totalRating/count;
+		}
+	}
+	
+	public ArrayList<Amenity> getAmenities() {
+		return Amenities;
+	}
 
-    public double getLocationX() {
-        return locationX;
-    }
+	public void setAmenities(ArrayList<Amenity> amenities) {
+		Amenities = amenities;
+	}
 
-    public void setLocationX(double locationX) {
-        this.locationX = locationX;
-    }
+	public void addAmenity(Amenity amenity) {
+		this.Amenities.add(amenity);
+	}
 
-    public double getLocationY() {
-        return locationY;
-    }
+	public ArrayList<Carpark> getNearbyCarparks() {
+		return NearbyCarparks;
+	}
 
-    public void setLocationY(double locationY) {
-        this.locationY = locationY;
-    }
+	public void setNearbyCarparks(ArrayList<Carpark> nearbyCarparks) {
+		NearbyCarparks = nearbyCarparks;
+	}
 
+	public void addCarpark(Carpark carpark) {
+		this.NearbyCarparks.add(carpark);
+	}
+
+	public ArrayList<Review> getReviews() {
+		return Reviews;
+	}
+	
+	public void setReviews(ArrayList<Review> reviews) {
+		Reviews = reviews;
+	}
+	
+	public void addReview(Review review) {
+		this.Reviews.add(review);
+		this.setOverallRating();
+	}
 }
-
