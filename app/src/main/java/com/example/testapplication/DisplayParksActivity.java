9adbe6c0910f29b11fffa5ce16d4f8df6985bcfc
Yilin <B180023@e.ntu.edu.sg>
2020-03-06 00:18:48 +0800

    6 Mar 00:18 by YL, Display Parks acitivity design and code,unfinished

--- app/src/main/java/com/example/testapplication/DisplayParksActivity.java
+++ app/src/main/java/com/example/testapplication/DisplayParksActivity.java
@@ -3,6 +3,12 @@ package com.example.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class DisplayParksActivity extends AppCompatActivity {
 
@@ -10,5 +16,41 @@ public class DisplayParksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_parks);
+
+
+
+        //////////////////////////////////////////////////////////
+        //finalise keyword, distance and rating filters input
+        Button listViewButton = findViewById(R.id.listViewButton);
+        Button mapViewButton = findViewById(R.id.mapViewButton);
+
+
+        listViewButton.setOnClickListener(new View.OnClickListener()) {
+            @Override
+            public void onClick(View v) {
+
+
+
+                //get keyword filter
+                EditText filterKeywordEditText = findViewById(R.id.filterKeywordEditText);
+                String keyword = filterKeywordEditText.getText().toString();
+
+                //get distance filter
+                SeekBar filterDistanceSeekBar = findViewById(R.id.filterDistanceSeekBar);
+                TextView distanceTextView = findViewById(R.id.distanceTextView);
+                int distance = filterDistanceSeekBar.getProgress();
+                distanceTextView.setText("Within\n" + distance + " km");
+
+                //get rating filter
+                RatingBar filterRatingBar = findViewById(R.id.filterRatingBar);
+                TextView ratingTextView = findViewById(R.id.ratingTextView);
+                ratingTextView.setText(filterRatingBar.getRating() + "");
+
+
+
+
+            }
+
+        }
     }
 }
