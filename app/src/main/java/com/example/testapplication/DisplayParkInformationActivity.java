13dc94aa8bfbb31be3348ef30ea538060402239b
Yilin <B180023@e.ntu.edu.sg>
2020-03-09 21:58:01 +0800

    9 Mar 22:00 by YL, fixed constraints in FilterActivity

--- app/src/main/java/com/example/testapplication/DisplayParkInformationActivity.java
+++ app/src/main/java/com/example/testapplication/DisplayParkInformationActivity.java
@@ -3,6 +3,7 @@ package com.example.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.SeekBar;
@@ -11,13 +12,15 @@ import android.widget.TextView;
 import com.example.testapplication.EntityClass.Park;
 import com.google.android.gms.maps.MapView;
 
+import java.util.Objects;
+
 public class DisplayParkInformationActivity<ParkName> extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_park_information);
 
-        Park park = getIntent().getExtras().getParcelable("PARK");
+        Park park = Objects.requireNonNull(getIntent().getExtras()).getParcelable("PARK");
 
         /*TextView textView = (TextView)findViewById(R.id.textView);
         textView.setText("AbhiAndroid"); //set text for text view */
@@ -25,47 +28,34 @@ public class DisplayParkInformationActivity<ParkName> extends AppCompatActivity 
         // Display park name
         TextView parkName = findViewById(R.id.parkName);
         parkName.setText(park.getName());
-
-        // Display park rating
-        TextView parkRating = findViewById(R.id.parkRating);
-        parkRating.setText((int) park.getOverallRating());
-
-        // Display park description
+//
+//        // Display park rating
+//        TextView parkRating = findViewById(R.id.parkRating);
+//        parkRating.setText((int) park.getOverallRating());
+//
+//        // Display park description
         TextView parkDescription = findViewById(R.id.parkDescription);
         parkDescription.setText(park.getDescription());
-
-        // Display park address
-        TextView parkAddress = findViewById(R.id.parkAddress);
-        parkAddress.setText(park.getLocationAddress());
-
-        // Display park website
-        TextView parkWebsite = findViewById(R.id.parkWebsite);
-        parkWebsite.setText(park.getWebsite());
-
-        // Display park location on google map
-        MapView googleMap = findViewById(R.id.googleMap);
-
-        // Share park
-        ImageButton sharePark = findViewById(R.id.sharePark);
-
-        // Favourite park
-        ImageButton favouritePark = findViewById(R.id.favouritePark);
-
-        // Review park
-        ImageButton reviewPark = findViewById(R.id.reviewPark);
+        parkDescription.setMovementMethod(new ScrollingMovementMethod());
+//
+//        // Display park address
+//        TextView parkAddress = findViewById(R.id.parkAddress);
+//        parkAddress.setText(park.getLocationAddress());
+//
+//        // Display park website
+//        TextView parkWebsite = findViewById(R.id.parkWebsite);
+//        parkWebsite.setText(park.getWebsite());
+//
+//        // Display park location on google map
+//        MapView googleMap = findViewById(R.id.googleMap);
+//
+//        // Share park
+//        ImageButton sharePark = findViewById(R.id.sharePark);
+//
+//        // Favourite park
+//        ImageButton favouritePark = findViewById(R.id.favouritePark);
+//
+//        // Review park
+//        ImageButton reviewPark = findViewById(R.id.reviewPark);
     }
 }
-
-/*    //get distance filter
-        SeekBar filterDistanceSeekBar = findViewById(R.id.filterDistanceSeekBar);
-        TextView distanceTextView = findViewById(R.id.distanceTextView);
-        int distanceF = filterDistanceSeekBar.getProgress();
-        distanceTextView.setText("Within\n" + distanceF + " km");
-
-                        //get rating filter
-                        RatingBar filterRatingBar = findViewById(R.id.filterRatingBar);
-                        TextView ratingTextView = findViewById(R.id.ratingTextView);
-                        double ratingF = filterRatingBar.getRating();
-                        ratingTextView.setText(String.format("%.1f",ratingF));
-
- */
